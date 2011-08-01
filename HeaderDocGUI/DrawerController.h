53c3624069896b81aaa6d4812388eac44eb4665e
Nate Beatty <nate.beatty@gmail.com>
2011-08-01 18:39:52 -0400

    Initial Commit 2

---
+++ HeaderDocGUI/DrawerController.h
@@ -0,0 +1,13 @@
+//
+//  DrawerController.h
+//  HeaderDoc GUI
+//
+//  Created by Nate Beatty on 8/1/11.
+//  Copyright 2011 MiddPoint. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface DrawerController : NSObject
+
+@end
---
+++ HeaderDocGUI/DrawerController.m
@@ -0,0 +1,23 @@
+//
+//  DrawerController.m
+//  HeaderDoc GUI
+//
+//  Created by Nate Beatty on 8/1/11.
+//  Copyright 2011 MiddPoint. All rights reserved.
+//
+
+#import "DrawerController.h"
+
+@implementation DrawerController
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
